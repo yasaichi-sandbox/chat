b115af82b2b86de772ca827713448a380f9e9fd4
yasaichi <yasaichi@users.noreply.github.com>
2018-02-11 00:35:15 +0900

    1.1.1 テンプレート

--- main.go
+++ main.go
@@ -3,21 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう!
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	// All fields are private because their names start with lower case
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join(
+			"templates",
+			t.filename,
+		)))
 	})
+
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
