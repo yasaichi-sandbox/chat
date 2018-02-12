73466767c9c3362b6d7f1f8a4c16a3e92f2e409f
yasaichi <yasaichi@users.noreply.github.com>
2018-02-12 21:35:48 +0900

    1.2.3

--- room.go
+++ room.go
@@ -6,3 +6,25 @@ type room struct {
 	leave   chan *client
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				// バッファに空きが出る前に送信 = 短時間での連続送信？でcloseしてしまうのはどうなのかな
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
