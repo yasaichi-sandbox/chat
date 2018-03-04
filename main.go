cbd376c1f3ba935b4356568b989ce3fb8c5b2a66
yasaichi <yasaichi@users.noreply.github.com>
2018-03-04 16:02:48 +0900

    3.1.4

--- main.go
+++ main.go
@@ -59,6 +59,17 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", room)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1, // To revoke this cookie instantly
+		})
+
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 
 	go room.run()
 
