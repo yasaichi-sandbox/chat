a01a0bdb7e5a5a3cc855dfddbf22e963e16b21e7
yasaichi <yasaichi@users.noreply.github.com>
2018-03-06 00:28:49 +0900

    3.2.1

---
+++ avatar.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"errors"
+)
+
+// ErrNoAvatarURL is an error raised when Avatar instance can't return the avatar URL.
+var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません")
+
+// Avatar is a type which represents a profile image of users.
+type Avatar interface {
+	// URL() returns the avatar URL of the specified client
+	// It returns an error when something goes wrong.
+	// Especially it does ErrNoAvatarURL when it can't fetch the avatar URL.
+	URL(c *client) (string, error)
+}
